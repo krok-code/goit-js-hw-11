4ad7f5f74c0ebf7a5af4f2b780c5f5ec103cde7d
krok-code <maks.smolinskyi@gmail.com>
2023-11-30 11:09:35 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 let currentPage = 1;
-const perPage = 40; // Змінено на 40 на сторінку
+const perPage = 40;
 
 document.getElementById('search-form').addEventListener('submit', async function (event) {
   event.preventDefault();
@@ -40,6 +40,16 @@ document.getElementById('search-form').addEventListener('submit', async function
       showLoadMoreButton();
     }
 
+    if (currentPage * perPage >= totalHits) {
+      hideLoadMoreButton();
+      Notiflix.Notify.info("You've reached the end of available images.");
+    }
+
+    // Очищення галереї при новому запиті
+    if (currentPage === 1) {
+      clearGallery();
+    }
+
     // Виклик методу refresh бібліотеки SimpleLightbox
     updateLightbox();
     // Плавне прокручування сторінки
@@ -64,7 +74,6 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 // Решта коду залишається незмінним
-
 
 async function fetchAndDisplayImages(searchQuery, page) {
   try {
@@ -93,57 +102,57 @@ function displayImages(images) {
   const gallery = document.getElementById('gallery');
 
   if (gallery) {
-   if (currentPage === 1) { 
-    gallery.innerHTML = ''; // Очистити галерею тільки при першому запиті
+    images.forEach(image => {
+      const photoCard = document.createElement('div');
+      photoCard.classList.add('photo-card');
+
+      const imgLink = document.createElement('a');
+      imgLink.href = image.largeImageURL;
+      imgLink.setAttribute('data-lightbox', 'gallery');
+
+      const img = document.createElement('img');
+      img.src = image.webformatURL;
+      img.alt = image.tags;
+      img.loading = 'lazy';
+
+      imgLink.appendChild(img);
+
+      const info = document.createElement('div');
+      info.classList.add('info');
+
+      const likes = document.createElement('p');
+      likes.classList.add('info-item');
+      likes.innerHTML = `<b>Likes</b> ${image.likes}`;
+
+      const views = document.createElement('p');
+      views.classList.add('info-item');
+      views.innerHTML = `<b>Views</b> ${image.views}`;
+
+      const comments = document.createElement('p');
+      comments.classList.add('info-item');
+      comments.innerHTML = `<b>Comments</b> ${image.comments}`;
+
+      const downloads = document.createElement('p');
+      downloads.classList.add('info-item');
+      downloads.innerHTML = `<b>Downloads</b> ${image.downloads}`;
+
+      info.appendChild(likes);
+      info.appendChild(views);
+      info.appendChild(comments);
+      info.appendChild(downloads);
+
+      photoCard.appendChild(imgLink);
+      photoCard.appendChild(info);
+
+      gallery.appendChild(photoCard);
+    });
+
+    if (currentPage * perPage >= totalHits) {
+      hideLoadMoreButton();
+    }
+  } else {
+    console.error("Gallery element not found.")
   }
-
-  images.forEach(image => {
-    const photoCard = document.createElement('div');
-    photoCard.classList.add('photo-card');
-
-    const imgLink = document.createElement('a');
-    imgLink.href = image.largeImageURL;
-    imgLink.setAttribute('data-lightbox', 'gallery');
-
-    const img = document.createElement('img');
-    img.src = image.webformatURL;
-    img.alt = image.tags;
-    img.loading = 'lazy';
-
-    imgLink.appendChild(img);
-
-    const info = document.createElement('div');
-    info.classList.add('info');
-
-    const likes = document.createElement('p');
-    likes.classList.add('info-item');
-    likes.innerHTML = `<b>Likes</b> ${image.likes}`;
-
-    const views = document.createElement('p');
-    views.classList.add('info-item');
-    views.innerHTML = `<b>Views</b> ${image.views}`;
-
-    const comments = document.createElement('p');
-    comments.classList.add('info-item');
-    comments.innerHTML = `<b>Comments</b> ${image.comments}`;
-
-    const downloads = document.createElement('p');
-    downloads.classList.add('info-item');
-    downloads.innerHTML = `<b>Downloads</b> ${image.downloads}`;
-
-    info.appendChild(likes);
-    info.appendChild(views);
-    info.appendChild(comments);
-    info.appendChild(downloads);
-
-    photoCard.appendChild(imgLink);
-    photoCard.appendChild(info);
-
-    gallery.appendChild(photoCard);
-  });
-} else {
-  console.error("Gallery element not found.")
-}
 }
 
 function hideLoadMoreButton() {
